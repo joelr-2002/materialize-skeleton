c7410ba88c453c2956d2d828ceeeb57adac89dc0
joelr-2002 <joelr-2002@users.noreply.github.com>
2026-04-21 05:25:00 +0000

    docs: add skeleton and cascader documentation

--- Gruntfile.js
+++ Gruntfile.js
@@ -446,7 +446,9 @@ module.exports = function(grunt) {
           'text-inputs.html': 'jade/text-inputs.jade',
           'support-us.html': 'jade/support-us.jade',
           'floating-action-button.html': 'jade/floating-action-button.jade',
-          'auto-init.html': 'jade/auto-init.jade'
+          'auto-init.html': 'jade/auto-init.jade',
+          'skeleton.html': 'jade/skeleton.jade',
+          'cascader.html': 'jade/cascader.jade'
         }
       }
     },
